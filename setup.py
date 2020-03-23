117c1545088691c4a4c3aaa617ded6eced909a98
kanishk307 <ysj30797.kj@gmail.com>
2020-03-23 00:14:55 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import csv
-from matplotlib import pyplot as plt
-from matplotlib import style
-import numpy as np
-#import plotly.express as px
